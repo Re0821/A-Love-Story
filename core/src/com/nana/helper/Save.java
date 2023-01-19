43f6ece670424bbe4ad7d7aaa36a34971042d11b
Re0821 <sean.yz.lee@gmail.com>
2023-01-18 19:30:04 -0500

    working on mac

---
+++ core/src/com/nana/helper/Save.java
@@ -0,0 +1,12 @@
+package com.nana.helper;
+
+import com.badlogic.gdx.Screen;
+
+public class Save {
+    private Screen currentScreen;
+    
+    public Save(Screen currentScreen){
+        this.currentScreen = currentScreen;
+    }
+    
+}
